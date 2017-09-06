985bde54b70e5941150719af4b6ff1f94b4918dd
JeremyMcCormick <jermccormick@gmail.com>
2017-09-06 12:57:11 -0700

    add simple example plot

--- src/main/java/org/hps/users/tvm/MCAnalysisDriver.java
+++ src/main/java/org/hps/users/tvm/MCAnalysisDriver.java
@@ -1,12 +1,22 @@
 package org.hps.users.tvm;
 
+import hep.aida.IHistogram1D;
+
+import java.util.List;
+
 import org.hps.analysis.examples.MCTruthExampleDriver;
 import org.lcsim.event.EventHeader;
+import org.lcsim.event.MCParticle;
 import org.lcsim.geometry.Detector;
 import org.lcsim.util.Driver;
+import org.lcsim.util.aida.AIDA;
 
 
 public class MCAnalysisDriver extends Driver {
+    
+    private static AIDA PLOT = AIDA.defaultInstance();
+    
+    private IHistogram1D particleEnergyH1D;
     
     public MCAnalysisDriver() {
         // add MC truth print as sub-driver
@@ -17,13 +27,25 @@ public class MCAnalysisDriver extends Driver {
         // detector changed hook
     }
     
-    public void startOfData() {
-        // start of data hook
+    public void startOfData() {    
+        particleEnergyH1D = PLOT.histogram1D(
+                "MCParticle Energy", /* plot name */ 
+                200 /* n bins */, 
+                0.0 /* lower edge */, 
+                2.0 /* upper edge */);    
     }
     
     public void process(EventHeader event) {
-        // process an event
-        super.process(event);        
+        // call child driver
+        super.process(event);     
+        
+        List<MCParticle> particles = event.get(
+                MCParticle.class /* type of object */, 
+                "MCParticle" /* collection name */);
+        
+        for (MCParticle p : particles) {
+            particleEnergyH1D.fill(p.getEnergy());
+        }
     }
 
 }
